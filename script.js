dab915d45dc41603e8f764b15a47acef446b4c61
Thais Hoshii <53278565+Thais-Hoshii@user.noreply.github.com>
2021-03-16 23:56:40 -0300

    Add code and properties to change message popup
    Added a code that detects whether the input is valid or not: if invalid, it will show the message popup in the normal color; if valid, it will change all the colors and the path icon. To change the path icon, it's better to just add another path tag and put the svg there, and hide it with display:none until necessary. Also added some ids to the new tags.

--- script.js
+++ script.js
@@ -3,3 +3,25 @@
 function $(txt) { return document.querySelector(txt); };
 
 $("form").reset();
+
+function checkEmail() {
+    let input = $("#email-input");
+    let popup = $("#signup-popup");
+
+    if (!input.checkValidity()) {
+        $("#signup-popup").style.position = "static";
+        input.value = "";
+        input.placeholder = "Invalid email input";
+    } else {
+        $(".email-form").style.display = "none";
+        popup.style.position = "static";
+        $("#signup-popup h1").innerText = "Yay! Thank you";
+        $("#signup-popup p").innerText = "We've sent a confirmation link to your inbox.";
+        $("#icon-error").style.display = "none";
+        $("#icon-check").style.display = "initial";
+        $("#popup-icon").style.fill = "var(--color-success)";
+        popup.style.borderColor = "var(--color-success)";
+        popup.style.background = "var(--color-success-bg)";
+        popup.style.color = "var(--color-success)";
+    }
+}
